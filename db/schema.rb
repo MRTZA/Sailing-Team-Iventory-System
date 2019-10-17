a32c39f6f25b1f690f3bb16222679df749cdd606
Jadie <jmar016@tamu.edu>
2019-10-16 22:56:04 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150809022253) do
-  /
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-  /
   create_table "inventory", force: :cascade do |t|
     t.string "item"
     t.integer "checked_out"
