faec2617a3b20dadf28aa72bd2d81ad43efd95b6
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2019-10-18 16:27:47 -0500

    frontend iteration 1

---
+++ app/helpers/item_helper.rb
@@ -0,0 +1,11 @@
+module ItemHelper
+  # Checks if a number is odd:
+  def oddness(count)
+    count.odd? ?  "odd" :  "even"
+  end
+
+  def self.all_options
+    ['All Turned In', 'All Checked Out', 'Any Checked Out']
+  end
+end
+
