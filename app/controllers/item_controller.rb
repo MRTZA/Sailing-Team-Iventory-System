5b383533657080b70fe2981f88902334d7806bff
Jadie <jmar016@tamu.edu>
2019-10-18 12:19:44 -0500

    Update item_controller.rb

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -12,21 +12,7 @@ class ItemController < ApplicationController
 
   def index
     @sort = params[:sort] || session[:sort]
-    #Ratings that are selected
-    @checked_options = params[:options] || session[:options] ||
-      Hash[ItemHelper.all_options.map {|k| [k, true]}]
-    #ALL ratings
-    @all_options = Hash[ItemHelper.all_options.map {|k| [k, @checked_options.key?(k)]}]
-    # Remembers the session - did this in Part 2. Didn't realize this would be
-    # part of Part 3 when I looked into doing the checkboxes and updating the list based on them.
-    # So I had already used [session] for the sort and ratings.
-    session[:sort] = @sort
-    session[:options] = @checked_options
-    if !params[:sort] && !params[:options]
-      flash.keep
-      redirect_to item_path(:options => @checked_options, :sort => @sort) and return
-    end
-    @item = Item.where(:options => @checked_options.keys).order @sort
+    
   end
 
   def new
