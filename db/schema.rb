7395527287c4f082e7c3c8eda6f2de2696e62e6f
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2019-10-18 16:27:47 -0500

    frontend iteration 1

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,11 @@
 ActiveRecord::Schema.define(version: 20150809022253) do
 
   create_table "items", force: :cascade do |t|
-    t.string   "item"
+    t.string   "title"
     t.text     "description"
+    t.integer  "checked_out"
     t.integer  "quantity"
+    t.integer  "remaining"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
