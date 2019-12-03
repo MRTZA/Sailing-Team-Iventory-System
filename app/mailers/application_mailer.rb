526aee85115f4dbe1b1e3fb311f0611bdc0f8bed
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2019-12-02 18:45:55 -0600

    notifaction system

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'me@sandboxfacd08cb7c3445c1b4409de917d5e15f.mailgun.org'
   layout 'mailer'
 end
---
+++ spec/mailers/previews/model_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/model_mailer
+class ModelMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/model_mailer/checkout_notification
+  def checkout_notification
+    ModelMailer.checkout_notification
+  end
+
+end
